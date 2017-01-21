66c5fb2772d5fb3278a4e7aec1fd8419d515e880
Tobias Widén <tobias.widen@trioptima.com>
2017-01-21 11:59:17 +0100

    Process supervisor

--- src/main/java/com/twiden/supervisor/Supervisor.java
+++ src/main/java/com/twiden/supervisor/Supervisor.java
@@ -1,57 +1,143 @@
 package com.twiden.supervisor;
 
-// import org.apache.http.*;
-
-// import org.apache.http.client.methods.HttpGet;
-// import org.apache.http.impl.client.DefaultHttpClient;
-// import org.apache.http.util.EntityUtils;
-
+import java.io.IOException;
+import org.apache.http.HttpHost;
+import org.apache.http.HttpResponse;
+import org.apache.http.HttpEntity;
+import org.apache.http.client.methods.HttpGet;
+import org.apache.http.client.methods.HttpPatch;
+import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.NoHttpResponseException;
+import org.apache.http.util.EntityUtils;
+import org.apache.http.entity.ByteArrayEntity;
+import org.json.simple.JSONArray;
+import org.json.simple.JSONObject;
+import org.json.simple.parser.JSONParser;
+import org.json.simple.parser.ParseException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.util.HashMap;
+import java.util.Map;
+import java.util.Iterator;
+import java.text.DateFormat;
+import java.text.SimpleDateFormat;
+import java.util.Date;
 
 public class Supervisor {
 
     public static void main(String[] args) {
         System.out.println("Supervisor!");
-        while (true) {
-            try {
-                Thread.sleep(60 * 1000);
-            } catch(InterruptedException ex) {
-                Thread.currentThread().interrupt();
-            }
-            System.out.println("Hello");
+
+        int sleep_seconds = 60;
+        if (System.getenv("SLEEP_SECONDS") != null) {
+            sleep_seconds = Integer.parseInt(System.getenv("SLEEP_SECONDS"));
         }
 
-  //   DefaultHttpClient httpclient = new DefaultHttpClient();
-  //   try {
-  //     // specify the host, protocol, and port
-  //     HttpHost target = new HttpHost("weather.yahooapis.com", 80, "http");
+        String backend_url = "http://localhost:8000/service/";
+        if (System.getenv("BACKEND_URL") != null) {
+            backend_url = System.getenv("BACKEND_URL");
+        }
 
-  //     // specify the get request
-  //     HttpGet getRequest = new HttpGet("/forecastrss?p=80020&u=f");
+        System.out.println("Backend URL: " + backend_url);
+        System.out.println("Sleep sleep seconds: " + sleep_seconds);
 
-  //     System.out.println("executing request to " + target);
+        boolean first_iteration = true;
+        while (true) {
 
-  //     HttpResponse httpResponse = httpclient.execute(target, getRequest);
-  //     HttpEntity entity = httpResponse.getEntity();
+            if (!first_iteration) {
+                try {
+                    Thread.sleep(sleep_seconds * 1000);
+                } catch(InterruptedException ex) {
+                    Thread.currentThread().interrupt();
+                }
+            }
+            first_iteration = false;
 
-  //     System.out.println("----------------------------------------");
-  //     System.out.println(httpResponse.getStatusLine());
-  //     Header[] headers = httpResponse.getAllHeaders();
-  //     for (int i = 0; i < headers.length; i++) {
-  //       System.out.println(headers[i]);
-  //     }
-  //     System.out.println("----------------------------------------");
+            healthCheck(backend_url);
+        }
+    }
 
-  //     if (entity != null) {
-  //       System.out.println(EntityUtils.toString(entity));
-  //     }
+    private static void healthCheck(String backend_url) {
+        HashMap<String, String> services = loadServices(backend_url);
+        Iterator it = services.entrySet().iterator();
+        while (it.hasNext()) {
+            Map.Entry entry = (Map.Entry)it.next();
+            String id = (String) entry.getKey();
+            String url = (String) entry.getValue();
+            boolean ok = checkServiceStatus(id, url);
+            updateServiceStatus(id, backend_url, ok);
+        }
+    }
 
-  //   } catch (Exception e) {
-  //     e.printStackTrace();
-  //   } finally {
-  //     // When HttpClient instance is no longer needed,
-  //     // shut down the connection manager to ensure
-  //     // immediate deallocation of all system resources
-  //     httpclient.getConnectionManager().shutdown();
-  //   }
-  }
+    private static HashMap<String, String> loadServices(String backend_url) {
+        String backend_response;
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+
+        try {
+            HttpResponse r = httpclient.execute(new HttpGet(backend_url));
+            HttpEntity entity = r.getEntity();
+            backend_response = EntityUtils.toString(entity);
+        } catch (IOException e) {
+            System.out.println("Seems like the backend is not responding...");
+            return new HashMap<String, String>();
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+
+        return parseServices(backend_response);
+    }
+
+    private static HashMap<String, String> parseServices(String backend_response) {
+        HashMap<String, String> services = new HashMap<>();
+
+        try {
+            JSONParser parser = new JSONParser();
+            Object obj = parser.parse(backend_response);
+            JSONObject jsonObject =  (JSONObject) obj;
+
+            for (Object s : (JSONArray) jsonObject.get("services")) {
+                JSONObject service = (JSONObject) s;
+                String id = (String) service.get("id");
+                String url = (String) service.get("url");
+                services.put(id, url);
+            }
+        } catch (ParseException e) {
+            System.out.println("Seems like the backend returned invalid json...");
+            return new HashMap<String, String>();
+        }
+        return services;
+    }
+
+    private static boolean checkServiceStatus(String id, String url) {
+        boolean success = true;
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        try {
+            HttpResponse ping = httpclient.execute(new HttpGet(url));
+            if (ping.getStatusLine().getStatusCode() != 200) {
+                success = false;
+            }
+        } catch (IOException e) {
+            success = false;
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+        return success;
+    }
+
+    private static void updateServiceStatus(String id, String backend_url, boolean success) {
+        DefaultHttpClient httpclient = new DefaultHttpClient();
+        DateFormat df = new SimpleDateFormat("yyyy-MM-dd HH:mm");
+        String now = df.format(new Date());
+        try {
+            HttpPatch patch = new HttpPatch(backend_url + id);
+            String json = "{\"status\": \"" + (success ? "OK" : "FAIL") + "\", \"timestamp\": \"" + now + "\"}";
+            HttpEntity entity = new ByteArrayEntity(json.getBytes("UTF-8"));
+            patch.setEntity(entity);
+            HttpResponse response = httpclient.execute(patch);
+        } catch (IOException e) {
+            System.out.println("Seems like the backend is not responding...");
+        } finally {
+            httpclient.getConnectionManager().shutdown();
+        }
+    }
 }
